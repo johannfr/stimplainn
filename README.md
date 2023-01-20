27bb997aaeb61f8e6f0f5d0e2f6b59c370b704b6
Jóhann Friðriksson <johann.fridriksson@gmail.com>
2023-01-20 11:49:36 +0100

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# stimplainn
+
+Apparat til að stimpla sig inn, já.
+
+## Uppsetning
+
+Mæli með uppsetningu gegnum pipX:
+```bash
+pip3 install --user pipx
+```
+
+Og svo setja inn pakkann sjálfan:
+
+```bash
+pipx install git+https://github.com/johannfr/stimplainn.git
+```
+
+## Notkun
+
+```bash
+stimplainn <starfsmannanumer> [-j tegund]
+```
+
+Tegundir eru þær sem sýnilegar eru í listanum á skráningarsíðunni, talið frá 0:
+  0. Vinna
+  1. Frí
+  2. Veikindi
+  3. Veikindi barns
+
+### Dæmi
+
+#### Stimpla inn/út í vinnu
+```bash
+stimplainn 1234567
+```
+#### Skrá veikindi
+```bash
+stimplainn 1234567 -j 2
+```
---
+++ stimplainn/main.py
@@ -0,0 +1,46 @@
+import click
+import requests
+from parsel import Selector
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+}
+URL = "http://klukka.dkvistun.is/menningarfelagakureyrar?Length=12"
+
+
+@click.command()
+@click.argument("employee_number")
+@click.option("-j", "--jobtype", default=0, show_default=True)
+def main(employee_number, jobtype):
+    content = requests.get(URL, headers=HEADERS).text
+
+    form_data = {
+        "__LASTFOCUS": "",
+        "__VIEWSTATE": "",
+        "__VIEWSTATEGENERATOR": "",
+        "__EVENTTARGET": "",
+        "__EVENTARGUMENT": "",
+        "ctl00$_LoginForm$ssn": "",
+        "ctl00$_LoginForm$jobtype": "",
+        "ctl00$_LoginForm$desc": "",
+        "ctl00$_LoginForm$hfGeo": "",
+    }
+
+    selector = Selector(content)
+    form_data["__VIEWSTATE"] = selector.css("#__VIEWSTATE").attrib["value"]
+    form_data["__VIEWSTATEGENERATOR"] = selector.css("#__VIEWSTATEGENERATOR").attrib[
+        "value"
+    ]
+    form_data["ctl00$_LoginForm$ssn"] = f"{employee_number}"
+    form_data["ctl00$_LoginForm$jobtype"] = f"{jobtype}"
+
+    post_response = requests.post(URL, data=form_data, headers=HEADERS).text
+    selector = Selector(post_response)
+    if (error := selector.css("#ctl00__LoginForm_lblError::text").get()) is not None:
+        click.echo(error, err=True)
+    else:
+        print(selector.css("#ctl00__LoginForm_lblSuccess::text").get())
+
+
+if __name__ == "__main__":
+    main()
